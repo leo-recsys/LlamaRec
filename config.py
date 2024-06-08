0ba1e78add6e0ab3c37ff66d5f3ebd086b9ff0b9
doojin <doojin88@gmail.com>
2024-06-08 20:04:20 +0900

    config 설정을 dict 로 정의할 수 있도록 수정

--- config.py
+++ config.py
@@ -1,8 +1,6 @@
-import numpy as np
-import random
-import torch
-import argparse
 
+import argparse
+import torch
 
 RAW_DATASET_ROOT_FOLDER = 'data'
 EXPERIMENT_ROOT = 'experiments'
@@ -22,10 +20,10 @@ def set_template(args):
     else:
         args.bert_max_len = 50
 
-    if 'llm' in args.model_code: 
+    if 'llm' in args.model_code:
         batch = 16 if args.dataset_code == 'ml-100k' else 12
         args.lora_micro_batch_size = batch
-    else: 
+    else:
         batch = 16 if args.dataset_code == 'ml-100k' else 64
 
     args.train_batch_size = batch
@@ -52,102 +50,71 @@ def set_template(args):
     args.bert_num_heads = 2
     args.bert_head_size = None
 
+args_dict = {
+    'dataset_code': None,
+    'min_rating': 0,
+    'min_uc': 5,
+    'min_sc': 5,
+    'seed': 42,
+    'train_batch_size': 64,
+    'val_batch_size': 64,
+    'test_batch_size': 64,
+    'num_workers': 8,
+    'sliding_window_size': 1.0,
+    'negative_sample_size': 10,
+    'device': 'cuda',
+    'num_epochs': 500,
+    'optimizer': 'AdamW',
+    'weight_decay': None,
+    'adam_epsilon': 1e-09,
+    'momentum': None,
+    'lr': 0.001,
+    'max_grad_norm': 5.0,
+    'enable_lr_schedule': True,
+    'decay_step': 10000,
+    'gamma': 1,
+    'enable_lr_warmup': True,
+    'warmup_steps': 100,
+    'val_strategy': 'iteration',
+    'val_iterations': 500,
+    'early_stopping': True,
+    'early_stopping_patience': 20,
+    'metric_ks': [ 1, 5, 10, 20, 50 ],
+    'rerank_metric_ks': [ 1, 5, 10 ],
+    'best_metric': 'Recall@10',
+    'rerank_best_metric': 'NDCG@10',
+    'use_wandb': False,
+    'model_code': None,
+    'bert_max_len': 50,
+    'bert_hidden_units': 64,
+    'bert_num_blocks': 2,
+    'bert_num_heads': 2,
+    'bert_head_size': 32,
+    'bert_dropout': 0.2,
+    'bert_attn_dropout': 0.2,
+    'bert_mask_prob': 0.25,
+    'llm_base_model': 'meta-llama/Llama-2-7b-hf',
+    'llm_base_tokenizer': 'meta-llama/Llama-2-7b-hf',
+    'llm_max_title_len': 32,
+    'llm_max_text_len': 1536,
+    'llm_max_history': 20,
+    'llm_train_on_inputs': False,
+    'llm_negative_sample_size': 19,
+    'llm_system_template': 'Given user history in chronological order, recommend an item from the candidate pool with its index letter.',
+    'llm_input_template': 'User history: {}; \n Candidate pool: {}',
+    'llm_load_in_4bit': True,
+    'llm_retrieved_path': None,
+    'llm_cache_dir': None,
+    'lora_r': 8,
+    'lora_alpha': 32,
+    'lora_dropout': 0.05,
+    'lora_target_modules': [ 'q_proj', 'v_proj' ],
+    'lora_num_epochs': 1,
+    'lora_val_iterations': 100,
+    'lora_early_stopping_patience': 20,
+    'lora_lr': 0.0001,
+    'lora_micro_batch_size': 16
+}
 
-parser = argparse.ArgumentParser()
-
-################
-# Dataset
-################
-parser.add_argument('--dataset_code', type=str, default=None)
-parser.add_argument('--min_rating', type=int, default=0)
-parser.add_argument('--min_uc', type=int, default=5)
-parser.add_argument('--min_sc', type=int, default=5)
-parser.add_argument('--seed', type=int, default=42)
-
-################
-# Dataloader
-################
-parser.add_argument('--train_batch_size', type=int, default=64)
-parser.add_argument('--val_batch_size', type=int, default=64)
-parser.add_argument('--test_batch_size', type=int, default=64)
-parser.add_argument('--num_workers', type=int, default=8)
-parser.add_argument('--sliding_window_size', type=float, default=1.0)
-parser.add_argument('--negative_sample_size', type=int, default=10)
-
-################
-# Trainer
-################
-# optimization #
-parser.add_argument('--device', type=str, default='cuda', choices=['cpu', 'cuda'])
-parser.add_argument('--num_epochs', type=int, default=500)
-parser.add_argument('--optimizer', type=str, default='AdamW', choices=['AdamW', 'Adam'])
-parser.add_argument('--weight_decay', type=float, default=None)
-parser.add_argument('--adam_epsilon', type=float, default=1e-9)
-parser.add_argument('--momentum', type=float, default=None)
-parser.add_argument('--lr', type=float, default=0.001)
-parser.add_argument('--max_grad_norm', type=float, default=5.0)
-parser.add_argument('--enable_lr_schedule', type=bool, default=True)
-parser.add_argument('--decay_step', type=int, default=10000)
-parser.add_argument('--gamma', type=float, default=1)
-parser.add_argument('--enable_lr_warmup', type=bool, default=True)
-parser.add_argument('--warmup_steps', type=int, default=100)
-
-# evaluation #
-parser.add_argument('--val_strategy', type=str, default='iteration', choices=['epoch', 'iteration'])
-parser.add_argument('--val_iterations', type=int, default=500)  # only for iteration val_strategy
-parser.add_argument('--early_stopping', type=bool, default=True)
-parser.add_argument('--early_stopping_patience', type=int, default=20)
-parser.add_argument('--metric_ks', nargs='+', type=int, default=[1, 5, 10, 20, 50])
-parser.add_argument('--rerank_metric_ks', nargs='+', type=int, default=[1, 5, 10])
-parser.add_argument('--best_metric', type=str, default='Recall@10')
-parser.add_argument('--rerank_best_metric', type=str, default='NDCG@10')
-parser.add_argument('--use_wandb', type=bool, default=False)
-
-################
-# Retriever Model
-################
-parser.add_argument('--model_code', type=str, default=None)
-parser.add_argument('--bert_max_len', type=int, default=50)
-parser.add_argument('--bert_hidden_units', type=int, default=64)
-parser.add_argument('--bert_num_blocks', type=int, default=2)
-parser.add_argument('--bert_num_heads', type=int, default=2)
-parser.add_argument('--bert_head_size', type=int, default=32)
-parser.add_argument('--bert_dropout', type=float, default=0.2)
-parser.add_argument('--bert_attn_dropout', type=float, default=0.2)
-parser.add_argument('--bert_mask_prob', type=float, default=0.25)
-
-################
-# LLM Model
-################
-parser.add_argument('--llm_base_model', type=str, default='meta-llama/Llama-2-7b-hf')
-parser.add_argument('--llm_base_tokenizer', type=str, default='meta-llama/Llama-2-7b-hf')
-parser.add_argument('--llm_max_title_len', type=int, default=32)
-parser.add_argument('--llm_max_text_len', type=int, default=1536)
-parser.add_argument('--llm_max_history', type=int, default=20)
-parser.add_argument('--llm_train_on_inputs', type=bool, default=False)
-parser.add_argument('--llm_negative_sample_size', type=int, default=19)  # 19 negative & 1 positive
-parser.add_argument('--llm_system_template', type=str,  # instruction
-    default="Given user history in chronological order, recommend an item from the candidate pool with its index letter.")
-parser.add_argument('--llm_input_template', type=str, \
-    default='User history: {}; \n Candidate pool: {}')
-parser.add_argument('--llm_load_in_4bit', type=bool, default=True)
-parser.add_argument('--llm_retrieved_path', type=str, default=None)
-parser.add_argument('--llm_cache_dir', type=str, default=None)
-
-################
-# Lora
-################
-parser.add_argument('--lora_r', type=int, default=8)
-parser.add_argument('--lora_alpha', type=int, default=32)
-parser.add_argument('--lora_dropout', type=float, default=0.05)
-parser.add_argument('--lora_target_modules', type=list, default=['q_proj', 'v_proj'])
-parser.add_argument('--lora_num_epochs', type=int, default=1)
-parser.add_argument('--lora_val_iterations', type=int, default=100)
-parser.add_argument('--lora_early_stopping_patience', type=int, default=20)
-parser.add_argument('--lora_lr', type=float, default=1e-4)
-parser.add_argument('--lora_micro_batch_size', type=int, default=16)
-
-################
-
-
-args = parser.parse_args()
+args = argparse.Namespace(**args_dict)
+print("args:", args)
